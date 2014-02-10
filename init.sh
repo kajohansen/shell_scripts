595bb4145f0a4babf3b708efb216a6f0e52c7e65
Karl Andreas <kajohansen.1@gmail.com>
2014-02-10 14:25:39 +0100

    Updated

--- init.sh
+++ init.sh
@@ -3,29 +3,28 @@
 # AWS initial setup script
 #
 
-# change host to google dns
-if sudo sed -i 's/127.0.0.1/8.8.8.8/g' /etc/resolv.conf
-then
-  cat /etc/resolv.conf
-else
-  echo "Error ..."
-  exit 1
-fi
+# change host to google dns and set hostname to mserve
+sudo sed -i 's/127.0.0.1/8.8.8.8/g' /etc/resolv.conf
+sudo sed -i 's/.*/mserve/g' /etc/hostname
+
 # check to see that the hostname got updated
-if grep '8.8.8.8' /etc/resolv.conf
+if grep '8.8.8.8' /etc/resolv.conf && grep 'mserve' /etc/hostname
 then
   echo "Got google host"
+	echo "Hostname is set to:" && hostname
 else
-  echo "error no host"
+  echo "error no host or no hostname..."
   exit 1
 fi
+
 # now update and upgrade system
 if sudo apt-get update
 then
 	sudo apt-get upgrade -y
 else
-	echo "Error.."
+	echo "Error.. updating system"
 fi
+
 # install dns-server
 if sudo tasksel install dns-server
 then
@@ -34,6 +33,7 @@ else
   echo "DNS server install failed.."
   exit 1
 fi
+
 # change host to localhost dns
 if sudo sed -i 's/8.8.8.8/127.0.0.1/g' /etc/resolv.conf
 then
@@ -44,3 +44,16 @@ else
   exit 1
 fi
 
+# install git
+if sudo apt-get install git -y
+then
+  echo "Git intstalled"
+	git clone https://github.com/kajohansen/my-zsh.git
+	echo "my-zsh installed"
+else
+	echo "Error installing git.."
+	exit 1
+fi
+
+# done restart
+sudo shutdown -r now
