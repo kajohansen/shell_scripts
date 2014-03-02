292f4d9ec6f636a4418e17979ac1a7ab368bdf12
Karl Andreas <kajohansen.1@gmail.com>
2014-03-02 10:56:40 +0100

    add nat, modified publickey and init

--- publickey.sh
+++ publickey.sh
@@ -2,25 +2,38 @@
 # create publickey for logging in to this system
 #
 
+
+# Check if we are on Mac or Linux
+if [ -d "/Users" ] ; then
+	echo "Mac Users directory exists"
+	DIR='Users'
+elif [ -d "/home" ] ; then
+	echo "Linux  /home directory exists"
+	DIR='home'
+else
+	echo "Neither home or Users directory where found"
+	exit 1
+fi
+
 # Get username and hostname, then create key and publickey
 if HOSTNAME=`hostname` && USERNAME=`whoami` && mkdir /home/$USERNAME/.ssh 
 then
   echo "Got hostname $HOSTNAME"
   echo "Got username $USERNAME"
-  echo "/home/$USERNAME/.ssh directory created"
-  if ssh-keygen -q -t rsa -b 4096 -C "Key for $USERNAME on $HOSTNAME" -f "/home/$USERNAME/.ssh/superuser-$HOSTNAME"
+  echo "/$DIR/$USERNAME/.ssh directory created"
+  if ssh-keygen -q -t rsa -b 4096 -C "Key for $USERNAME on $HOSTNAME" -f "/home/$USERNAME/.ssh/$USERNAME-$HOSTNAME"
   then
-    echo "/home/$SUPERUSER/.ssh/superuser-$HOSTNAME created successfuly"
+    echo "/$DIR/$USERNAME/.ssh/superuser-$HOSTNAME created successfuly"
   else
    echo "Error creating /home/$USERNAME/.ssh/superuser-$HOSTNAME"
   fi
 else
-  echo "Error /home/$USERNAME/.ssh directory not created"
+  echo "Error /$DIR/$USERNAME/.ssh directory not created"
   exit 1
 fi
 
 # add key to authorized_keys file
-if cat /home/$USERNAME/.ssh/$USERNAME-$HOSTNAME.pub  >> /home/$USERNAME/.ssh/authorized_keys
+if cat /$DIR/$USERNAME/.ssh/$USERNAME-$HOSTNAME.pub  >> /home/$USERNAME/.ssh/authorized_keys
 then
   echo ".ssh authorized_keys file created"
 else
